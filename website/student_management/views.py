2f36d308b2b1f9dc97928c3994bb408b6bd60e53
Vigneshwar Ravichandran <vigneshwarravichandran@gmail.com>
2019-10-02 23:12:14 +0530

    Login Features
    Signed-off-by: Vigneshwar Ravichandran <vigneshwarravichandran@gmail.com>

--- website/student_management/views.py
+++ website/student_management/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate
 
-# Create your views here.
+def login(request):
+	context = {
+		'error' : None,
+	}
+	if request.method == 'POST':
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+		user = authenticate(username=username, password=password)
+		if user is not None:
+			return redirect(user, userid)
+		context['error'] = 'Invalid credentials'
+		return render(request, 'login.html', context)
+	return render(request, 'login.html', context)
